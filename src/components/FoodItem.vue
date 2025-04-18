293fc7ce42d8f57ed70ff883b7920a834b6fa484
kinungipaul21 <kinungipaul21@gmail.com>
2025-04-18 16:21:01 +0300

    doing some project

--- src/components/FoodItem.vue
+++ src/components/FoodItem.vue
@@ -1,15 +1,27 @@
 <template>
-  <h1>
-    {{ message }}
-  </h1>
+  <div>
+    <h2>{{ foodName }}</h2>
+    <h2>{{ foodDesc }}</h2>
+    <p>Is this your Favorite?
+      {{ isFav.toString() }}
+    </p>
+    <button @click="togglefav">Favorite</button>
+  </div>
 </template>
 
 <script>
 export default{
+  props:['foodName', 'foodDesc', 'favorite'],
   data(){
     return{
-      message: "Hello Pikay!"
+      isFav: this.favorite
+    }
+  },
+  methods:{
+    togglefav(event){
+      this.isFav=!this.isFav;
     }
   }
 }
+
 </script>
