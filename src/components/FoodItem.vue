aa1eb5f725226355d35c122001b14b391e3ff275
kinungipaul21 <kinungipaul21@gmail.com>
2025-04-18 15:21:31 +0300

    Hello Pikay

--- src/components/FoodItem.vue
+++ src/components/FoodItem.vue
@@ -1,56 +1,15 @@
 <template>
-<h2>
-{{foodName}}
-</h2>
-  <div>
-    {{foodDesc}}<br/>
-    <button @click="changeFav"> Mark Favorite</button>
-    {{foodisFavorite.toString()}}
-  </div>
+  <h1>
+    {{ message }}
+  </h1>
 </template>
 
 <script>
 export default{
-  props:{
-    foodName:{
-      type:String,
-      required:true
-    },
-    foodDesc:{
-      type:String,
-      required:false,
-      default:'This is the default description',
-      validator:function(value){
-        if(20<value.length && value.length<50){
-          return true;
-        }
-        else{
-          return false;
-        }
-      }
-    },
-    isFavorite:{
-      type:Boolean,
-      required:false,
-      default:false
-    }
-  },
   data(){
     return{
-      foodisFavorite:this.isFavorite
-    }
-  },
-  methods:{
-    changeFav(event){
-      this.foodisFavorite =!this.foodisFavorite;
+      message: "Hello Pikay!"
     }
   }
 }
 </script>
-<style>
-img{
-  height:1.5rem;
-  width:3rem;
-  float:right;
-}
-</style>
